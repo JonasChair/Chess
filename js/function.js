c932177b3f0139551feb6b91632bb46d65a15d36
Jonas Chairutdinovas <jonas.chairutdinovas@gmail.com>
2019-06-06 12:10:49 +0300

    add full board setup

--- js/function.js
+++ js/function.js
@@ -10,9 +10,21 @@ function resetBoard(){
         columns = row.querySelectorAll('div');
         switch (row_index) {
             case '8':                
-                columns.forEach(x => {               
+                columns.forEach(x => {
                     if (x.attributes['col-index'].value === 'A' || x.attributes['col-index'].value === 'H'){
                         x.innerHTML = '<i class="fas fa-chess-rook black"></i>';
+                    }
+                    if (x.attributes['col-index'].value === 'B' || x.attributes['col-index'].value === 'G'){
+                        x.innerHTML = '<i class="fas fa-chess-knight black"></i>';
+                    }
+                    if (x.attributes['col-index'].value === 'C' || x.attributes['col-index'].value === 'F'){
+                        x.innerHTML = '<i class="fas fa-chess-bishop black"></i>';
+                    }
+                    if (x.attributes['col-index'].value === 'D'){
+                        x.innerHTML = '<i class="fas fa-chess-queen black"></i>';
+                    }
+                    if (x.attributes['col-index'].value === 'E'){
+                        x.innerHTML = '<i class="fas fa-chess-king black"></i>';
                     }
                 });
                 break;
@@ -26,6 +38,18 @@ function resetBoard(){
                 columns.forEach(x => {               
                     if (x.attributes['col-index'].value === 'A' || x.attributes['col-index'].value === 'H'){
                         x.innerHTML = '<i class="fas fa-chess-rook white"></i>';
+                    }
+                    if (x.attributes['col-index'].value === 'B' || x.attributes['col-index'].value === 'G'){
+                        x.innerHTML = '<i class="fas fa-chess-knight white"></i>';
+                    }
+                    if (x.attributes['col-index'].value === 'C' || x.attributes['col-index'].value === 'F'){
+                        x.innerHTML = '<i class="fas fa-chess-bishop white"></i>';
+                    }
+                    if (x.attributes['col-index'].value === 'D'){
+                        x.innerHTML = '<i class="fas fa-chess-queen white"></i>';
+                    }
+                    if (x.attributes['col-index'].value === 'E'){
+                        x.innerHTML = '<i class="fas fa-chess-king white"></i>';
                     }
                 });
                 break;
