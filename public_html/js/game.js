4a3e92c4e64da75f2116779f68671256fe4f940d
Jonas Chairutdinovas <jonas.chairutdinovas@gmail.com>
2019-09-04 12:54:45 +0300

    refactor game into one js

--- public_html/js/game.js
+++ public_html/js/game.js
@@ -1,5 +1,55 @@
 'use strict';
 
+var states = {
+    start: 'start',
+    inProgress: 'in progress',
+    promotion: 'promotion',
+    draw: 'draw',
+    blackWin: 'blackWin',
+    whiteWin: 'whiteWin'
+}
+
+var colors = {
+    white : 'white',
+    black : 'black',
+    none : ''
+}
+
+var peaces = {
+    pawn : 'pawn',
+    knight : 'knight',
+    bishop : 'bishop',
+    rook : 'rook',
+    queen : 'queen',
+    king : 'king',
+    empty : ''
+}
+
+var horizontal = ['A','B','C','D','E','F','G','H'];
+
+var selectedPeace = {
+        row: '',
+        col: '',
+        isSelected: false
+}
+var promoted = {
+        row: -1,
+        col: -1
+}
+
+var highlighted = [];
+
+var path = [];
+
+var castle = [];
+
+var gameBoard = {
+    turn: 'white',
+    state: 'start',
+    board:
+        [[],[],[],[],[],[],[],[]]
+}
+
 function addEventListeners(data, type, funct) {
     data.forEach(x => x.addEventListener(type,funct));
 }
@@ -574,3 +624,7 @@ function renderBoard() {
     
     return;
 }
+
+initBoard();
+
+renderBoard();
